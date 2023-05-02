a56e99880b4b7393b7228d4acbd218d639d59d60
M Hanif <hanif>
2023-05-02 03:27:38 -0700

    npm build

--- src/Api/Axios.js
+++ src/Api/Axios.js
@@ -3,5 +3,5 @@ import axios from "axios";
 export default axios.create({
   // baseURL: "http://127.0.0.1:8000/api",
   // baseURL: 'http://192.168.100.14:8080/api/'
-  baseURL: "https://zendleykiosk.walnuthash.com/api/",
+  baseURL: "http://zendleykiosk.walnuthash.com/api/",
 });
